f924d6978311fcfe89c70567b22e1826e2cc8b59
sinan-marayi <sinanmarayi8@gmail.com>
2022-12-24 11:14:19 +0530

    font setup

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,28 @@
 </template>
 
 <script>
-import './assets/tailwind.css';
-import Navbar from './components/navbar.vue';
-import Home from './pages/Home.vue';
+import "./assets/tailwind.css";
+import Navbar from "./components/navbar.vue";
+import Home from "./pages/Home.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Navbar,
-    Home
-  }
-}
+    Home,
+  },
+};
 </script>
 
 <style>
-
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+a {
+  font-family: "inter";
+}
 </style>
--- src/components/cards/apartmentCard.vue
+++ src/components/cards/apartmentCard.vue
@@ -3,7 +3,7 @@
                 <div class="flex">
                     <img src="../../assets/img/card/Rectangle42strip-image-02.png" class="mr-3" alt="">
                     <div class="flex flex-col">
-                        <span class="mt-1 text-sm font-light">Apartment/Condo</span>
+                        <span class="mt-1 text-sm font-inter">Apartment/Condo</span>
                         <h3 class="text-base font-medium mt-1 leading-5">Kelana Mahkota Condominium
                         </h3>
                         <h4 class="text-sm font-light">Chinatown, Kuala lumpur</h4>
